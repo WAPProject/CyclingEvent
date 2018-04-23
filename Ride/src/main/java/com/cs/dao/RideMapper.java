19ca8341e96c1634e7dd2f62581feed995326f70
Yanlong <wangyanlong0107@gmail.com>
2018-04-23 13:05:03 -0500

    import

--- Ride/src/main/java/com/cs/dao/RideMapper.java
+++ Ride/src/main/java/com/cs/dao/RideMapper.java
@@ -16,10 +16,10 @@ public interface RideMapper {
 	*@Date:4/22/2018_11:42 PM
 	*@Description: list ride by status and order
 	*/
-	@Select("select * from Ride where status=#{status} order by begindate ${order}")
+	@Select("select * from ride where status=#{status} order by begindate ${order}")
 	public List<Ride> listRideByStatus(@Param("status")String status, @Param("order")String order);
 
-	@Select("SELECT rideid ,r.route,r.currentlocation,concat (\"\",GROUP_CONCAT(`name`),\"\") as paticipants , concat(\"\", GROUP_CONCAT(u.id), \"\") AS paticipantids FROM USER u LEFT JOIN user_ride ur ON u.id = ur.userid LEFT JOIN Ride r ON ur.rideid = r.id WHERE r. STATUS = 'flag' group by rideid order by r.begindate ${order}")
+	@Select("SELECT rideid ,r.route,r.currentlocation,concat (\"\",GROUP_CONCAT(`name`),\"\") as paticipants , concat(\"\", GROUP_CONCAT(u.id), \"\") AS paticipantids FROM user u LEFT JOIN user_ride ur ON u.id = ur.userid LEFT JOIN ride r ON ur.rideid = r.id WHERE r. STATUS = 'flag' group by rideid order by r.begindate ${order}")
 	public List<Message> listFlagUserAndMsg(@Param("order")String order);
 
 	/**
