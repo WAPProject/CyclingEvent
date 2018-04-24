24eb4c62d93b3e44d0a4dfa3b19a2b9475b416a9
Yanlong <wangyanlong0107@gmail.com>
2018-04-25 02:12:35 +0800

    Merge branch 'master' of https://github.com/WAPProject/CyclingEvent
    # Conflicts:
    #	Ride/web/homepage.jsp

--- Ride/src/main/resources/db.sql
+++ Ride/src/main/resources/db.sql
@@ -13,10 +13,23 @@ route varchar(128) ,
 creatoruserid varchar(64),
 status varchar(64),
 currentlocation varchar(64),
-begindate timestamp
+begindate timestamp,
+banner varchar(64)
 );
-INSERT INTO `ride` VALUES (1, 'F to O', '1', 'notstart', NULL, '2018-5-1 01:59:48');
-INSERT INTO `ride` VALUES (2, 'Ottumwa to Demoine', '1', 'notstart', NULL, '2018-5-5 02:00:36');
+INSERT INTO `ride` VALUES (1, 'F to O', '1', 'flag', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (2, 'Ottumwa to Demoine', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (9, '123', '1', 'end', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (10, 'michael s 2 ride', '2', 'flag', NULL, '2018-4-24 13:49:31', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (11, '22222 ride', '1', 'flag', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (12, 'asd', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (13, NULL, '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (14, NULL, '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (15, '123123', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (16, '123123', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (17, '123123', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (18, '213123123', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+INSERT INTO `ride` VALUES (19, '到让2', '1', 'notstart', NULL, '2018-4-24 12:35:56', 'http://albert6.com:9999/uploadfile/48200205a5d54b5ba02c88219b095277.png');
+
 create table user_ride(
 id int primary key auto_increment,
 userid varchar(64),
